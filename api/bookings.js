8e0e45593c876e983383b6198ae6d995a003534a
rthan99 <nathanpottier@gmail.com>
2026-05-01 09:16:46 +0200

    Keys

--- api/bookings.js
+++ api/bookings.js
@@ -1,5 +1,5 @@
-const SUPABASE_URL = process.env.SUPABASE_URL;
-const SUPABASE_KEY = process.env.SUPABASE_KEY;
+const SUPABASE_URL = process.env.https://fivenrdkveorbfvqhwtn.supabase.co;
+const SUPABASE_KEY = process.env.sb_publishable_eNUrjAklGHV3fo7LgiwoNQ_gVlWk-x6;
 
 function jsonHeaders() {
   return {
